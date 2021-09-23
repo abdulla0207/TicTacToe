9eea0eef8bec687793623caa4562c2c23b9c00b7
Abdulla Ermatov <abdulla.ermatov0407@gmail.com>
2021-09-23 22:49:46 +0500

    Added a function to prevent filling the computer with an element
    This function saves all positions that the user and the computer took and prevents the cpu from filling up with an element in the same position

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -21,11 +23,23 @@ public class Main {
 
         int rand;
         int placePiece;
-        while(true) {
+        List<Integer> resultHolder = new ArrayList<>();
+
+        while(resultHolder.size() < 10) {
             System.out.println("Choose number from 1-9");
             placePiece = scanner.nextInt();
+            resultHolder.add(placePiece);
             input.userInput(b, placePiece, "player");
             rand = random.nextInt(9) + 1;
+            for (int i = 0; true; i++) {
+                if (resultHolder.contains(rand)) {
+                    System.out.println("This place is taken");
+                    rand = random.nextInt(9) + 1;
+                }else{
+                    break;
+                }
+            }
+            resultHolder.add(rand);
             input.userInput(b, rand, "computer");
             board.print(b);
         }
--- src/UserInput.java
+++ src/UserInput.java
@@ -1,70 +1,78 @@
+import java.util.Random;
+
 public class UserInput {
 
     public void userInput(char[][] b, int placePiece, String user) {
-       char symbol = 'c';
+        Random random = new Random();
+       char characterXorO = 'c';
+       if(user.equals("player")) {
+           characterXorO = 'X';
+       }else{
+           characterXorO = 'O';
+       }
 
         switch (placePiece) {
             case 1:
                 if(user.equals("player")){
-                    b[0][0] = 'X';
+                    b[0][0] = characterXorO;
                 }else if(user.equals("computer")){
-                    b[0][0] = 'O';
+                    b[0][0] = characterXorO;
                 }
                 break;
             case 2:
                 if(user.equals("player")) {
-                    b[0][2] = 'X';
+                    b[0][2] = characterXorO;
                 }else if(user.equals("computer")) {
-                    b[0][2] = 'O';
+                    b[0][2] = characterXorO;
                 }
                 break;
             case 3:
                 if(user.equals("player")) {
-                    b[0][4] = 'X';
+                    b[0][4] = characterXorO;
                 }else if(user.equals("computer")) {
-                    b[0][4] = 'O';
+                    b[0][4] = characterXorO;
                 }
                 break;
             case 4:
                 if(user.equals("player")) {
-                    b[2][0] = 'X';
+                    b[2][0] = characterXorO;
                 }else if(user.equals("computer")) {
-                    b[2][0] = 'O';
+                    b[2][0] = characterXorO;
                 }
                 break;
             case 5:
                 if(user.equals("player")) {
-                    b[2][2] = 'X';
+                    b[2][2] = characterXorO;
                 }else if(user.equals("computer")) {
-                    b[2][2] = 'O';
+                    b[2][2] = characterXorO;
                 }
                 break;
             case 6:
                 if(user.equals("player")) {
-                    b[2][4] = 'X';
+                    b[2][4] = characterXorO;
                 }else if(user.equals("computer")) {
-                    b[2][4] = 'O';
+                    b[2][4] = characterXorO;
                 }
                 break;
             case 7:
                 if(user.equals("player")) {
-                    b[4][0] = 'X';
+                    b[4][0] = characterXorO;
                 }else if(user.equals("computer")) {
-                    b[4][0] = 'O';
+                    b[4][0] = characterXorO;
                 }
                 break;
             case 8:
                 if(user.equals("player")) {
-                    b[4][2] = 'X';
+                    b[4][2] = characterXorO;
                 }else if(user.equals("computer")) {
-                    b[4][2] = 'O';
+                    b[4][2] = characterXorO;
                 }
                 break;
             case 9:
                 if(user.equals("player")) {
-                    b[4][4] = 'X';
+                    b[4][4] = characterXorO;
                 }else if(user.equals("computer")) {
-                    b[4][4] = 'O';
+                    b[4][4] = characterXorO;
                 }
                 break;
             default:
